cc2f523720f76b896b158547781b417f0349337d
Michael Peritore <primalcodes@gmail.com>
2021-09-16 20:04:49 -0400

    using post to accomodate for FE lack of ability to use PUT

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  post '/invoices/:id', to: 'invoices#update'
   resources :invoices do
+    post '/line_items/:id', to: 'line_items#update'
     resources :line_items
-  end
+  end  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
